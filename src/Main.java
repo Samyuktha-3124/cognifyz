23a2e9b5af3418944b1aa95236bac2210c983145
Samyuktha-3124 <samyukthasubash2431@gmail.com>
2025-03-10 12:55:12 +0530

    git commit -m "added"

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        int a=6;
+        int b=5;
+        int c=a+b;
+        System.out.print(c);
+    }
+}
